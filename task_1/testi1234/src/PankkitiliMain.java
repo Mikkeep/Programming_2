1baa08b07a3a8c46939972656b5387d43f151151
Mikkeep <mikke.pennanen@gmail.com>
2022-01-17 13:09:19 +0200

    Moved all prints to main from pankkitili class

--- task_1/testi1234/src/PankkitiliMain.java
+++ task_1/testi1234/src/PankkitiliMain.java
@@ -4,13 +4,19 @@ public class PankkitiliMain {
     public static void main(String[] args) {
         Pankkitili bank = new Pankkitili("", "", 0);
         Scanner input = new Scanner(System.in);
+        double tempBal;
         System.out.println("Enter account owners name please: ");
         bank.setOwner(input.nextLine());
         System.out.println("Enter account number please: ");
         bank.setAccNum(input.nextLine());
         System.out.println("Enter account balance please: ");
-        bank.setBalance(input.nextDouble());
-        bank.checkInfo();
+        tempBal = input.nextDouble();
+        if (tempBal < 0)
+            System.out.println("Balance cannot be a negative number\nBalance defaults to 0 until correct deposit\n");
+        else bank.setBalance(tempBal);
+        System.out.println("Account is owned by: " + bank.getUser());
+        System.out.println("Account number is : " + bank.getNum());
+        System.out.println("Account balance is : " + bank.getBal() + " €");
 
         while (true) {
             System.out.println("Choose an option to either withdraw or deposit money from your account");
@@ -24,15 +30,29 @@ public class PankkitiliMain {
                 switch (choice) {
                     case 1 -> {
                         System.out.println("Enter the deposit amount please: ");
-                        bank.setDeposit(input.nextDouble());
-                        bank.checkInfo();
+                        //tempBal variable reuse to collect input
+                        tempBal = input.nextDouble();
+                        if (tempBal < 0)
+                            System.out.println("Deposit cannot be a negative");
+                        else bank.setDeposit(tempBal);
+                        System.out.println("Account balance is : " + bank.getBal() + " €");
                     }
                     case 2 -> {
                         System.out.println("Enter the amount to withdraw please: ");
-                        bank.withdraw(input.nextDouble());
-                        bank.checkInfo();
+                        tempBal = input.nextDouble();
+                        if (tempBal > bank.getBal())
+                            System.out.println("Withdraw cannot be larger than the balance of the account");
+                        else if (tempBal < 0) {
+                            System.out.println("Withdraw cannot be negative");
+                        }
+                        else bank.withdraw(tempBal);
+                        System.out.println("Account balance is : " + bank.getBal() + " €");
                     }
-                    case 3 -> bank.checkInfo();
+                    case 3 -> {
+                        System.out.println("Account is owned by: " + bank.getUser());
+                        System.out.println("Account number is : " + bank.getNum());
+                        System.out.println("Account balance is : " + bank.getBal() + " €");
+                    }
                     case 4 -> {
                         System.out.println("Quitting the program, thank you for visiting!");
                         System.exit(0);
